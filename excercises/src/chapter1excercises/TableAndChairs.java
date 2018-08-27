7436df81f57ee7fd40309ffa022cd9d9298379c2
Cheyenne Jones <cj1011012020@gmail.com>
2018-08-27 09:33:13 -0500

    Table and Chair

--- excercises/src/chapter1excercises/TableAndChairs.java
+++ excercises/src/chapter1excercises/TableAndChairs.java
@@ -4,7 +4,36 @@ public class TableAndChairs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		System.out.println("X                       X");
+		System.out.println("X                       X");
+		System.out.println("X                       X");
+		System.out.println("X                       X");
+		System.out.println("X      XXXXXXXXXX       X");
+		System.out.println("X      X        X       X");
+		System.out.println("XXXX   X        X    XXXX");
+		System.out.println("X  X                 X  X");
+		
+		displayTruck();
+		
 	}
 
+	public static void displayTruck() { 
+
+		System.out.println("                                ");
+		System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("  XX                           X");
+		System.out.println("  XX                           X");
+		System.out.println("XX                             X");
+		System.out.println("X                              X");
+		System.out.println("X                              X");
+		System.out.println(" XX                            X");
+		System.out.println("   XXXXXXXXOOOOXXXXXXOOOOXXXXXXX");
+		System.out.println("          O    O    O    O      ");
+		System.out.println("           OOOO      OOOO       ");
+	
+	
+	
+	}
 }
