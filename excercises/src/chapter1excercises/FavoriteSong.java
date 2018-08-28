cc0a825cd7a823df0af969e77dd050b66141f843
Cheyenne Jones <cj1011012020@gmail.com>
2018-08-28 09:05:44 -0500

    Added FavoriteSong

---
+++ excercises/src/chapter1excercises/FavoriteSong.java
@@ -0,0 +1,15 @@
+package chapter1excercises;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Im tired of sleeping on us");
+		System.out.println("Im over dodging this crush");
+		System.out.println("Im sick of waiting ten minutes just to text you back");
+		System.out.println("So you dont get to thinkin that im too attached");
+
+	}
+
+}
