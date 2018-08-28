af1ea83543195d262a7fce6b2e1ad3938fff9a98
Cheyenne Jones <cj1011012020@gmail.com>
2018-08-28 09:26:50 -0500

    Added and fixed FixDebugOne4

---
+++ excercises/src/chapter1excercises/FixDebugOne4.java
@@ -0,0 +1,14 @@
+package chapter1excercises;
+
+import javax.swing.JOptionPane;
+
+public class FixDebugOne4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 JOptionPane.showMessageDialog(null, "First GUI program");
+
+	}
+
+}
