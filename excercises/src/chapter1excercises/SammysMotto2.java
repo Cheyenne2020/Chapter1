70cc13a777735f90e8500147d3dedc4d52af8660
Cheyenne Jones <cj1011012020@gmail.com>
2018-08-28 09:36:51 -0500

    Added SammysMotto2

---
+++ excercises/src/chapter1excercises/SammysMotto2.java
@@ -0,0 +1,18 @@
+package chapter1excercises;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		 System.out.println("S                                     S");
+		 System.out.println("S                                     S");
+		 System.out.println("S   Sammy's makes it fun in the sun   S");
+		 System.out.println("S                                     S");
+		 System.out.println("S                                     S");
+		 System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
+	}
+
+}
