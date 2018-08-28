e9218215c6b6e9aeaafbfdad9864568f8ebf3655
Cheyenne Jones <cj1011012020@gmail.com>
2018-08-28 09:17:17 -0500

    Added FixDebugOne1

---
+++ excercises/src/chapter1excercises/FixDebugOne1.java
@@ -0,0 +1,13 @@
+package chapter1excercises;
+
+public class FixDebugOne1 {
+	 
+	/* This program displays a greeting */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");	
+
+	}
+
+}
