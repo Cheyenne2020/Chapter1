4c80d17151606868a6ac22001d0065ddc770d951
Cheyenne Jones <cj1011012020@gmail.com>
2018-08-28 09:00:40 -0500

    Added MovieQuoteInfo

---
+++ excercises/src/chapter1excercises/MovieQuoteInfo.java
@@ -0,0 +1,16 @@
+package chapter1excercises;
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("You dont understand!");
+		System.out.println("From the movie On the Waterfront");
+		System.out.println("Edie Doyle said it");
+		System.out.println("The year of the movie is 1954");
+		
+
+	}
+
+}
