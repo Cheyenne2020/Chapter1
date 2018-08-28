02665c74771c2bc8677efc221c09b1a70ae982fa
Cheyenne Jones <cj1011012020@gmail.com>
2018-08-28 08:49:21 -0500

    Added my favorite movie quote

--- excercises/src/chapter1excercises/MoiveQuote.java
+++ excercises/src/chapter1excercises/MoiveQuote.java
@@ -1,9 +1,13 @@
 package chapter1excercises;
 
+import javax.swing.JOptionPane;
+
 public class MoiveQuote {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null,"You dont understand!");
 
 	}
 
