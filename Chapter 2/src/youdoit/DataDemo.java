4af9cb33c30110f3195a55e44490cee5e3a80a0e
Cheyenne Jones <cj1011012020@gmail.com>
2018-08-29 09:18:30 -0500

    Added DataDemo

--- Chapter 2/src/youdoit/DataDemo.java
+++ Chapter 2/src/youdoit/DataDemo.java
@@ -4,6 +4,11 @@ public class DataDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int aWholeNumber = 315; 
+		
+		System.out.print("The number is ");
+		System.out.println(aWholeNumber);
 
 	}
 
