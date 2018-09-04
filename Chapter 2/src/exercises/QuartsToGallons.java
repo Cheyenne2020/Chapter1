6ee521bbae6db888d1c31e1b0592fa967650cd89
Cheyenne Jones <cj1011012020@gmail.com>
2018-09-04 09:53:54 -0500

    added info

--- Chapter 2/src/exercises/QuartsToGallons.java
+++ Chapter 2/src/exercises/QuartsToGallons.java
@@ -4,6 +4,14 @@ public class QuartsToGallons {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int quartsRemaining; 
+		total gallons; 
+		int quartsGivenByUser = 18; 
+		int QuartsInGallons = 4; 
+		
+		total gallons = quartsRemaining/
+		
 
 	}
 
