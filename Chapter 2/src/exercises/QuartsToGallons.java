54d47339927a7a2cb648d2ac3a4595e0ffc7e772
Cheyenne Jones <cj1011012020@gmail.com>
2018-09-05 09:00:17 -0500

    Changed the int and added system

--- Chapter 2/src/exercises/QuartsToGallons.java
+++ Chapter 2/src/exercises/QuartsToGallons.java
@@ -5,14 +5,18 @@ public class QuartsToGallons {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int quartsRemaining; 
-		total gallons; 
-		int quartsGivenByUser = 18; 
-		int QuartsInGallons = 4; 
+		int numberOfGallons; 
+		int numberOfQuartsRemaining; 
+		int numberOfQuartsNeeded = 18; 
+		int Quarts_In_Gallons = 4; 
 		
-		total gallons = quartsRemaining/
+		numberOfGallons = numberOfQuartsNeeded / Quarts_In_Gallons; 
+		numberOfQuartsRemaining = numberOfQuartsNeeded % Quarts_In_Gallons; 
 		
-
-	}
+		System.out.print("The total amount of gallons is " + numberOfGallons); 
+		
+		System.out.print(" The total amount of quarts is " + numberOfQuartsRemaining); 
+	
+			}
 
 }
