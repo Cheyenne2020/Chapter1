f7bac12d01cba3bd4c5f5e06515e2da812954618
Cheyenne Jones <cj1011012020@gmail.com>
2018-09-06 09:51:29 -0500

    Added code

--- Chapter 2/src/exercises/InchesToFeet.java
+++ Chapter 2/src/exercises/InchesToFeet.java
@@ -4,6 +4,19 @@ public class InchesToFeet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int numberOfFeet; 
+		int numberOfInchesRemaining; 
+		int numberOfInchesNeeded = 86; 
+		int Inches_In_Feet = 12; 
+		
+		numberOfFeet = numberOfInchesNeeded / Inches_In_Feet; 
+		numberOfInchesRemaining = numberOfInchesNeeded % Inches_In_Feet; 
+		
+		System.out.print("The total amount of feet is " + numberOfFeet); 
+		
+		System.out.print(" The total amount of inches is " + numberOfInchesRemaining);
+		
 
 	}
 
