707041ae28e091ac18810123fa705473e0ab905a
Cheyenne Jones <cj1011012020@gmail.com>
2018-09-07 09:05:13 -0500

    Added code and Renamed

--- Chapter 2/src/exercises/InchesToFeetInteractive.java
+++ Chapter 2/src/exercises/InchesToFeetInteractive.java
@@ -1,6 +1,8 @@
 package exercises;
 
-public class InchesToFeet {
+import java.util.Scanner;
+
+public class InchesToFeetInteractive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,11 +12,15 @@ public class InchesToFeet {
 		int numberOfInchesNeeded = 86; 
 		int Inches_In_Feet = 12; 
 		
+		Scanner input = new Scanner(System.in);
+		
 		numberOfFeet = numberOfInchesNeeded / Inches_In_Feet; 
 		numberOfInchesRemaining = numberOfInchesNeeded % Inches_In_Feet; 
 		
-		System.out.print("The total amount of feet is " + numberOfFeet); 
+		System.out.print("Please enter an inch >> "); 
+		numberOfInchesNeeded = input.numberOfInchesRemaining(); 
 		
+		System.out.print("The total amount of feet is " + numberOfFeet); 
 		System.out.print(" The total amount of inches is " + numberOfInchesRemaining);
 		
 
